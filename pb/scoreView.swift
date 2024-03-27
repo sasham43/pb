32a8ce14f1db30805a9c91b6f2d5759b27e4312c
Sasha Kramer <sashasemail@gmail.com>
2024-03-27 16:20:26 -0400

    watching scores

--- pb/scoreView.swift
+++ pb/scoreView.swift
@@ -12,33 +12,55 @@ struct scoreView: View {
     @State private var homeScore : Int = 0
     @State private var awayScore = 0
     
+    @State private var status = ""
+    
     var body: some View {
-        Text("Score")
-        GeometryReader { geometry in
-            HStack {
-                VStack {
-                    Text("Home: \(homeScore)")
-                    Button("+") {
-                        homeScore += 1
+        VStack {
+            Text("Score")
+            Text("\(status)")
+            GeometryReader { geometry in
+                HStack {
+                    VStack {
+                        Text("Home: \(homeScore)")
+                        Button("+") {
+                            homeScore += 1
+                        }
+                        Button("-") {
+                            homeScore -= 1
+                        }
                     }
-                    Button("-") {
-                        homeScore -= 1
+                    .padding(10)
+                    VStack {
+                        Text("Away: \(awayScore)")
+                        Button("+") {
+                            awayScore += 1
+                        }
+                        Button("-") {
+                            awayScore -= 1
+                        }
                     }
                 }
-                .padding(10)
-                VStack {
-                    Text("Away: \(awayScore)")
-                    Button("+") {
-                        awayScore += 1
-                    }
-                    Button("-") {
-                        awayScore -= 1
-                    }
+                .frame(minWidth: geometry.size.width)
+            }
+            .onChange(of: homeScore) {
+                if(homeScore >= 11 && homeScore - awayScore >= 2) {
+                    print("Home wins")
+                    status = "Home wins"
                 }
             }
-            .frame(minWidth: geometry.size.width)
+            .onChange(of: awayScore) {
+                if(awayScore >= 11 && awayScore - homeScore >= 2) {
+                    print("Away wins")
+                    status = "Away wins"
+                }
+            }
+            Button("Reset") {
+                homeScore = 0
+                awayScore = 0
+                status = ""
+            }
+            Spacer()
         }
-        
         
     }
 }
